1940816f1b653af8fef3995443f73dc2fdc40924
adandoang <akhdanjzif@gmail.com>
2023-03-24 10:16:10 +0700

    procedure input

--- InsertionSort_174/InsertionSort_174.cpp
+++ InsertionSort_174/InsertionSort_174.cpp
@@ -4,6 +4,20 @@ using namespace std;
 int arr[20];
 int n;
 
-void input( {
+void input() {
 	while (true)
-	})
+	{
+		cout << "masukan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+
+		}
+		else
+		{
+			cout << "\nArray yang anda masukan maksimal 20 elemen.\n"
+		}
+	}
+}
+	
